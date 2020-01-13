0219cb8eba5de5c9b13c3b6636338084289cb324
Cherkesky <guy@cherkesky.com>
2020-01-13 16:58:39 -0600

    completed the exercise

--- cars.py
+++ cars.py
@@ -6,3 +6,17 @@ showroom.add("Fisker")
 showroom.add("Fisker")
 
 print(len(showroom))
+
+moreCars = {"Mercedes Benz", "Dacia"}
+showroom.update(moreCars)
+showroom.discard("BMW")
+print(f'Showroom {showroom}')
+
+junkyard = {"Hyundai", "Renualt", "Citruen", "Subaru", "Mazda", "Fisker"}
+print(f'Junkyard {junkyard}')
+
+intersectionCars = showroom.intersection(junkyard)
+print (f'Cross Inventory{intersectionCars}')
+
+showroom.discard("Mazda")
+print(f'Showroom2 {showroom}')
